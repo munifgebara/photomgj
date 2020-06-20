ef4dc8e13e8ae26fe61c042f0b63522120e1bce0
Munif <munifgebara@gmail.com>
2020-06-20 03:29:17 -0300

    first

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {
   Plugins, CameraResultType, Capacitor, FilesystemDirectory,
-  CameraPhoto, CameraSource
+  CameraPhoto, CameraSource, GeolocationPosition
 } from '@capacitor/core';
 
 
@@ -9,8 +9,7 @@ import { Platform } from '@ionic/angular';
 
 
 
-
-const { Camera, Filesystem, Storage } = Plugins;
+const { Camera, Filesystem, Storage,Geolocation } = Plugins;
 
 @Injectable({
   providedIn: 'root'
@@ -22,41 +21,51 @@ export class PhotoService {
   public photos: Photo[] = [];
 
   private PHOTO_STORAGE: string = "photos";
+  
+  public coordinates: GeolocationPosition;
 
   constructor(platform: Platform) {
     this.platform = platform;
+    this.getCurrentPosition();
   }
 
-// Save picture to file on device
-private async savePicture(cameraPhoto: CameraPhoto) {
-  // Convert photo to base64 format, required by Filesystem API to save
-  const base64Data = await this.readAsBase64(cameraPhoto);
-
-  // Write the file to the data directory
-  const fileName = new Date().getTime() + '.jpeg';
-  const savedFile = await Filesystem.writeFile({
-    path: fileName,
-    data: base64Data,
-    directory: FilesystemDirectory.Data
-  });
-
-  if (this.platform.is('hybrid')) {
-    // Display the new image by rewriting the 'file://' path to HTTP
-    // Details: https://ionicframework.com/docs/building/webview#file-protocol
-    return {
-      filepath: savedFile.uri,
-      webviewPath: Capacitor.convertFileSrc(savedFile.uri),
-    };
+  async getCurrentPosition() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    this.coordinates=coordinates;
+    console.log('Current', coordinates);
   }
-  else {
-    // Use webPath to display the new image instead of base64 since it's
-    // already loaded into memory
-    return {
-      filepath: fileName,
-      webviewPath: cameraPhoto.webPath
-    };
+
+  // Save picture to file on device
+  private async savePicture(cameraPhoto: CameraPhoto) {
+
+    // Convert photo to base64 format, required by Filesystem API to save
+    const base64Data = await this.readAsBase64(cameraPhoto);
+
+    // Write the file to the data directory
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: FilesystemDirectory.Data
+    });
+
+    if (this.platform.is('hybrid')) {
+      // Display the new image by rewriting the 'file://' path to HTTP
+      // Details: https://ionicframework.com/docs/building/webview#file-protocol
+      return {
+        filepath: savedFile.uri,
+        webviewPath: Capacitor.convertFileSrc(savedFile.uri),
+      };
+    }
+    else {
+      // Use webPath to display the new image instead of base64 since it's
+      // already loaded into memory
+      return {
+        filepath: fileName,
+        webviewPath: cameraPhoto.webPath
+      };
+    }
   }
-}
   public async addNewToGallery() {
     // Take a photo
     const capturedPhoto = await Camera.getPhoto({
@@ -75,15 +84,15 @@ private async savePicture(cameraPhoto: CameraPhoto) {
     Storage.set({
       key: this.PHOTO_STORAGE,
       value: this.platform.is('hybrid')
-              ? JSON.stringify(this.photos)
-              : JSON.stringify(this.photos.map(p => {
-                // Don't save the base64 representation of the photo data,
-                // since it's already saved on the Filesystem
-                const photoCopy = { ...p };
-                delete photoCopy.base64;
-    
-                return photoCopy;
-            }))
+        ? JSON.stringify(this.photos)
+        : JSON.stringify(this.photos.map(p => {
+          // Don't save the base64 representation of the photo data,
+          // since it's already saved on the Filesystem
+          const photoCopy = { ...p };
+          delete photoCopy.base64;
+
+          return photoCopy;
+        }))
     });
   }
 
@@ -91,7 +100,7 @@ private async savePicture(cameraPhoto: CameraPhoto) {
     // Retrieve cached photo array data
     const photos = await Storage.get({ key: this.PHOTO_STORAGE });
     this.photos = JSON.parse(photos.value) || [];
-  
+
     // Easiest way to detect when running on the web:
     // “when the platform is NOT hybrid, do this”
     if (!this.platform.is('hybrid')) {
@@ -99,10 +108,10 @@ private async savePicture(cameraPhoto: CameraPhoto) {
       for (let photo of this.photos) {
         // Read each saved photo's data from the Filesystem
         const readFile = await Filesystem.readFile({
-            path: photo.filepath,
-            directory: FilesystemDirectory.Data
+          path: photo.filepath,
+          directory: FilesystemDirectory.Data
         });
-  
+
         // Web platform only: Save the photo into the base64 field
         photo.base64 = `data:image/jpeg;base64,${readFile.data}`;
       }
@@ -116,14 +125,14 @@ private async savePicture(cameraPhoto: CameraPhoto) {
       const file = await Filesystem.readFile({
         path: cameraPhoto.path
       });
-  
+
       return file.data;
     }
     else {
       // Fetch the photo, read as a blob, then convert to base64 format
       const response = await fetch(cameraPhoto.webPath);
       const blob = await response.blob();
-  
+
       return await this.convertBlobToBase64(blob) as string;
     }
   }
